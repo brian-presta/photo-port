b96e85e3b37a064aca4c8d58b5cb1db12af911bd
brian presta <brian.presta@gmail.comgit commit -m first>
2020-09-28 14:42:03 -0700

    set up test for About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import About from '..'
+
+afterEach(cleanup)
+
+describe('About component', function() {
+    test('renders', () => {
+        render(<About />)
+    })
+    test("matches snapshot DOM node structure", () => {
+        const { asFragment } = render(<About />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+
+})
